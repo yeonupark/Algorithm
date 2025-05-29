dcb2eadab52525c81be6e782ef8317aa39a15cb7
yeonupark <idepix@naver.com>
2025-05-29 21:19:49 +0900

    update

--- leetcode/predictTheWinner.py
+++ leetcode/predictTheWinner.py
@@ -2,16 +2,16 @@ class Solution:
     def predictTheWinner(self, nums):
 
         def choose(nums, turn = True):
-        
-            if len(nums) < 3:
-                if turn:
-                    return max(nums)
-                else:
-                    return min(nums)
-                
+            
+            if len(nums) == 1:
+                return nums[0]
+            
             if turn:
-                return max(nums[0] + choose(nums[1:], False), nums[-1] + choose(nums[:len(nums)-1], False))
+                return max(nums[0] + choose(nums[1:], False), 
+                        nums[-1] + choose(nums[:len(nums)-1], False))
             else:
-                return min(choose(nums[1:], True), choose(nums[:len(nums)-1], True))
+                return min(choose(nums[1:], True), 
+                        choose(nums[:len(nums)-1], True))
+
 
         return sum(nums) - choose(nums)*2 <= 0
